165dad8c1cce8d4858d01af3666d623d5cf7d14e
AmberVanLoo <49272482+AmberVanLoo@users.noreply.github.com>
2019-04-11 16:15:02 +0200

    Create formChangePw.php

---
+++ formChangePw.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="changepassword.php">
+    <p><input type="password" name="oldpasswd" id="oldpasswd" maxlength="30" placeholder="Old Password"></p>
+    <p><input type="password" name="newpasswd1" id="newpasswd1" maxlength="30" placeholder="New Password"></p>
+    <p><input type="password" name="newpasswd2" id="newpasswd2"maxlength="30" placeholder="Confirm Password"></p>
+    <input type="submit" name="submit" id="submit" value="change password">
+    </form>  
+</body>
+</html>
+
+
