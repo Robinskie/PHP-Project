6c996db67de73a2266aa83c8ab84d87e6f39c983
AmberVanLoo <49272482+AmberVanLoo@users.noreply.github.com>
2019-04-20 16:38:57 +0200

    Update like.js

--- PHP-Project/like.js
+++ PHP-Project/like.js
@@ -1,7 +1,9 @@
 
 	// include ook jquery voor dat je dit include
 
-	 	//als er op de like btn is geklikt
+	var likeUnlike = 0;
+
+		//als er op de like btn word geklikt
 		$("a.like").on("click", function(e){// e staat voor event
 			//op welke post is er geklikt?
 			var postId = $(this).data('id');
@@ -9,21 +11,31 @@
 			var elLikes = $(this).parent().find(".likes");
 			var likes = elLikes.html();
 			
-			//is er al eens geklikt?
-				// @to do: unlike
-			//like saven
-			$.ajax({
-  				method: "POST", //post (=set), ge creeert data bij
-  				url: "ajax/like.php", //pagina om da te verwerken
-  				data: { postId: postId }, //postId meegeven, ma ge kunt eender welke data meegeven
-				dataType: "Json" // de server ga json terugggeven
-			})
-  				.done(function( res ) {
-    			if( res.status == "succes" ) {
-					likes++;
-					elLikes.html(likes);
-				}
-  			});
-			e.preventDefault();//ge ga ni meer naar boven in de pagina springen 
+
+				$.ajax({
+					method: "POST", //post (=set), ge creeert data bij
+					url: "ajax/like.php", //pagina om da te verwerken
+					data: { postId: postId }, //postId meegeven, ma ge kunt eender welke data meegeven
+					dataType: "Json" // de server ga json terugggeven
+				})
+				.done(function(res) {
+					//er is nog niet geliked
+					if (likeUnlike === 0) {
+						if( res.status == "succes" ) {
+						likes++;
+						elLikes.html(likes);
+						likeUnlike = 1;
+						e.preventDefault();//ge ga ni meer naar boven in de pagina springen 
+						}
+					} 
+					//er is al geliked
+					else {
+						if( res.status == "succes" ) {
+						likes--;
+						elLikes.html(likes);
+						likeUnlike = 0;
+						e.preventDefault();//ge ga ni meer naar boven in de pagina springen 
+						}
+					}
+				});
 		})
-		
