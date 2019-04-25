040a6461c65cb43c9e026bc487d44570ce6cd1d7
jonas vanhamme <jonas.vanhamme@hotmail.com>
2019-04-25 12:34:48 +0200

    Update index.php

--- PHP-Project/index.php
+++ PHP-Project/index.php
@@ -33,52 +33,6 @@
     </form>
     <!-- einde formulier -->
 
-    <!-- hoe werkt een zoekfunctie 
-    De bezoeker geeft een waarde in en drukt op de knop,
-    formulier gaat een go aan het einde van de query string toevoegen
-    -->
-
-    <!-- toevoegen van php voor zoekformulier -->
-    <?php
-    // is er op de submit knop geklikt
-    if(isset($_POST['submit'])){ 
-
-    // heeft de query string een go waarde 
-    if(isset($_GET['go'])){
-
-    // sql injectie tegengaan 
-    // enkel hoofdletter of kleine letter invullen als eerste karakter
-  
-    if(preg_match("/^[  a-zA-Z]+/", 
-    $_POST['name'])){ //  naam van het veld dat we willen checken
-    $name=$_POST['name']; // verzamelen van zoek criteria
-
-    // database connection met variabele db en ingebouwde functie myqsl_connect
-    $db=mysql_connect("mysql:host=localhost;dbname=project", "root",  "root") or die ('I cannot connect to the database  because: ' . mysql_error());
-  
-    // moet kunnen zoeken op #tags en zoekwoorden 
-    $sql="SELECT  ID, FirstName, LastName FROM Contacts WHERE FirstName LIKE '%" . $name .  "%' OR LastName LIKE '%" . $name ."%'";
-    $result=mysql_query($sql);
-
-    // loop door de zoekresultaten
-    while($row=mysql_fetch_array($result)){
-          $FirstName  =$row['FirstName'];
-          $LastName=$row['LastName'];
-          $ID=$row['ID'];
-
-    // toon resultaten in een array
-    echo "<ul>\n";
-    echo "<li>" . "<a  href=\"search.php?id=$ID\">"   .$FirstName . " " . $LastName .  "</a></li>\n";
-    echo "</ul>";
-    }
-    }
-    else{
-    echo  "<p> Vul het zoekveld in om een gebruiker te zoeken </p>";
-  }
-  }
-  }
-?>
-
 
     <h2><a href="uploadPhoto.php">Upload a picture</a></h2>
     </div>
