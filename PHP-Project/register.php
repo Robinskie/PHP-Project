7c869eb846a9cdf30599f6b5aecd3c6218809668
jonas vanhamme <jonas.vanhamme@hotmail.com>
2019-05-02 00:52:17 +0200

    Update register.php

--- PHP-Project/register.php
+++ PHP-Project/register.php
@@ -76,7 +76,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/styleLogin.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,900" rel="stylesheet">
     <title>PROJECT</title>
 </head>
