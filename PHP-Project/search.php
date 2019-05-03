c2492aea5f905136d322731f5f342bb2569d567d
sarahvandenheuvel <r0709458@student.thomasmore.be>
2019-05-03 16:17:46 +0200

    Comments
    Toevoegen comments

--- PHP-Project/search.php
+++ PHP-Project/search.php
@@ -1,7 +1,9 @@
 <?php
+
     require_once 'bootstrap.php';
     redirectIfLoggedOut();
 
+    // we roepen de functie searchPhotos aan in klasse Search
     if (!empty($_GET)) {
         $foundPhotos = Search::searchPhotos($_GET['search']);
     }
@@ -13,10 +15,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/style.css">
-    <title>PROJECT</title>
+    <title>PROJECT</title> 
 
 </head>
 <body>
+    <!-- navigatie toevoegen -->
     <?php include_once 'includes/nav.inc.php'; ?>
     
     <h1>Search results</h1>
@@ -27,19 +30,27 @@
 <!-- klik je op een resultaat, dan krijg je de detailpagina te zien met commentaren -->
 <?php foreach ($foundPhotos as $foundPhoto):
 
+            // object aanmaken
             $photo = new Photo();
             $photo->setId($foundPhoto['id']);
             $photo->setData();
 
             $uploadUser = $photo->getUploaderObject();
+            // we halen de getter uit de klasse photo
             $likeCount = $photo->getLikeCount();
             ?>
         
+            <!-- we printen een foto --> 
             <div class="photoBox">
+            <!-- we halen de getter uit klasse foto --> 
             <a href="photo.php?id=<?php echo $photo->getId(); ?>">
+            <!-- we halen getter uit klasse foto --> 
             <h3><?php echo $photo->getName(); ?></h3>
+            <!-- we halen getter uit klasse foto --> 
             <img src="images/photos/<?php echo $photo->getId(); ?>_cropped.png" width="300px"> 
+            <!-- we halen de naam uit de klasse user --> 
             <p><i><?php echo $uploadUser->getFullName(); ?></i></p>
+            <!-- we halen de upload data uit de klasse foto --> 
             <p class="photoDate"><?php echo howLongAgo(strtotime($photo->getUploadDate())); ?></p>
             <p><span class="likeCount"><?php echo $likeCount; ?></span> people like this</p>
                     
