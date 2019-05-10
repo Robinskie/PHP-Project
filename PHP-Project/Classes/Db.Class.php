f1da91506aca6ae4344257c30f3ffa5335d453dc
jonas vanhamme <jonas.vanhamme@hotmail.com>
2019-05-10 16:08:04 +0200

    Show followed tags in feed (in progress)

--- PHP-Project/Classes/Db.Class.php
+++ PHP-Project/Classes/Db.Class.php
@@ -43,8 +43,8 @@
             $postLimit = $to - $from;
             $conn = Db::getInstance();
             $statement = $conn->prepare(
-                'SELECT *, photos.id AS pId, users.id AS uId FROM photos 
-                LEFT JOIN users ON photos.uploader = users.id 
+                'SELECT *, photos.id AS pId, users.id AS uId FROM photos
+                LEFT JOIN users ON photos.uploader = users.id
                 RIGHT JOIN followers ON followers.followedUser = photos.uploader
                 WHERE followers.followingUser = :currentUser
                 ORDER BY uploadDate DESC
@@ -53,9 +53,55 @@
             $result = $statement->execute();
 
             return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            /*
+            // FEED MET POSTS VAN FOLLOWED USERS EN TAGS
+            $conn = Db::getInstance();
+            $statement = $conn->prepare('SELECT tagName FROM tags WHERE followingUser = '.$userId);
+            $statement->execute();
+            $tags = $statement->fetchAll(PDO::FETCH_ASSOC);
+            //var_dump($tags);
+
+            $queryStart = 'SELECT *, photos.id AS pId, users.id AS uId FROM photos
+            LEFT JOIN users ON photos.uploader = users.id
+            RIGHT JOIN followers ON followers.followedUser = photos.uploader
+            WHERE followers.followingUser = :currentUser ';
+
+            //var_dump($queryStart);
+
+            $count = 0;
+
+            foreach ($tags as $tag) {
+                $tagName = $tag['tagName'];
+                $queryName = 'query'.$count;
+                //var_dump($tagName);
+                $$queryName = 'UNION
+                SELECT * FROM photos WHERE description LIKE %'.$tagName.'% ';
+                ++$count;
+            }
+
+            //var_dump($query2);
+
+            $queryEnd = 'ORDER BY uploadDate DESC
+            LIMIT '.$postLimit.' OFFSET '.$from;
+
+            //var_dump($queryEnd);
+
+            $finalQuery = $queryStart.$queryEnd;
+
+            var_dump($finalQuery);
+
+            $conn = Db::getInstance();
+            $statement = $conn->prepare($finalQuery);
+            $statement->bindParam(':currentUser', $userId);
+            $statement->execute();
+
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            */
         }
 
-        public function getRandomOtherUser($userId)
+        public static function getRandomOtherUser($userId)
         {
             $conn = Db::getInstance();
             $randomUserStatement = $conn->prepare("SELECT id FROM users WHERE NOT id = $userId ORDER BY RAND() LIMIT 1");
